ddd1a640a17b97974591e5708d464dc0d6dd1bb3
vrglhuhu <cheanbvergel@gmail.com>
2023-07-01 20:32:01 +0800

    Psuedocodes for fan_main.py.

--- fan/fan_main.py
+++ fan/fan_main.py
@@ -1,2 +1,18 @@
 # Vergel, Chean Bernard Villanueva
 # Fan Class
+
+# Create a class called fan.
+# Create 3 constants with values of 1,2, and 3.
+
+# Accessor Methods
+# Make a private integer method for speed.
+# Make a private boolean method to on the fan.
+# Make a private float method for the radius of the fan.
+# Make a private string method for the color of the fan.
+
+
+# Mutator Methods  
+# Make a mutator method for speed.
+# Make a mutator method to on the fan.
+# Make a mutator method for the radius of the fan.
+# Make a mutator method for the color of the fan.
