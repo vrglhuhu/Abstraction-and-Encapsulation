b961ae22c6c9c93bb9cbf6c5541a6a67bf99baa1
vrglhuhu <cheanbvergel@gmail.com>
2023-07-01 20:54:12 +0800

    Making the accessor methods

--- fan/fan_main.py
+++ fan/fan_main.py
@@ -3,7 +3,7 @@
 
 # Create a class called fan.
 class Fan:
-    
+
 # Create 3 constants with values of 1,2, and 3.
     SLOW = 1
     MEDIUM = 2
@@ -11,10 +11,20 @@ class Fan:
 
 # Accessor Methods
 # Make a private integer method for speed.
+    def get_speed(self):
+        return self._speed
+    
 # Make a private boolean method to on the fan.
+    def get_on(self):
+        return self._on
+    
 # Make a private float method for the radius of the fan.
+    def get_radius(self):
+        return self._radius
+    
 # Make a private string method for the color of the fan.
-
+    def get_color(self):
+        return self._color
 
 # Mutator Methods  
 # Make a mutator method for speed.
