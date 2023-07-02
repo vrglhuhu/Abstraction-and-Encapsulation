0ecd63b742412dc334064ede048e1e76316c0b2d
vrglhuhu <cheanbvergel@gmail.com>
2023-07-02 14:50:58 +0800

    Filling the psuedocodes.

--- car/car_test.py
+++ car/car_test.py
@@ -2,6 +2,26 @@
 # Car Class
 
 # Import the car
+from car_main import Car
+
 # Creating a class for car test
+class Car_test():
+    car = Car(2023, "Example Make")
+
+    # Print the initial speed
+    print("\nInitial Speed:", car.get_speed())
+
     # Accelerating the car five times
+    for _ in range(5):
+        car.accelerate()
+        print("After Acceleration")
+        print("\nCurrent Speed:", car.get_speed())
+
     # Braking the car five times
+    for _ in range(5):
+        car.brake()
+        print("After Braking")
+        print("\nCurrent Speed:", car.get_speed())
+
+if __name__ == "__main__":
+    Car_test()
