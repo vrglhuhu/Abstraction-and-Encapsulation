f5f6d4c69e308dbac24929634c5c66a28b289e9a
vrglhuhu <cheanbvergel@gmail.com>
2023-07-02 14:24:33 +0800

    Psuedocode for car_main file.

---
+++ car/car_main.py
@@ -0,0 +1,11 @@
+# Vergel, Chean Bernard Villanueva
+# Car Class
+
+# Create a class called Car
+    # Make the data atrributes
+    # Create an accelerate method
+    # Create a brake method
+    # Create a speed method
+
+   
+        
