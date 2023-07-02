4acde8fc395075322a90c6ac3434f4368911285a
vrglhuhu <cheanbvergel@gmail.com>
2023-07-02 17:04:31 +0800

    Psuedocodes for pet.

---
+++ pet/pet.py
@@ -0,0 +1,21 @@
+# Vergel, Chean Bernard Villanueva
+# Pet Class
+
+# Write a class named Pet
+
+    # _ _name (for the name of a pet)
+    # _ _animal_type (for the type of animal that a pet is. Example values are ‘Dog’, ‘Cat’, and ‘Bird’)
+    # _ _age (for the pet’s age)
+    # The Pet class should have an _ _init_ _ method that creates these attributes. It should also have the following methods:
+
+    # Set_name() This method assigns a value to the _ _name field.
+
+    # Set_animal_type() This method assigns a value to the _ _animal_type field.
+
+    # Set_age() This method assigns a value to the _ _age field.
+
+    # Get_name() This method returns the value of the _ _ name field.
+
+    # get_animal_type() This method returns the value of the _ _animal_type field
+
+    # get_age() This method returns the value of the _ _age field.
---
+++ pet/pet_main.py
@@ -0,0 +1,2 @@
+# Vergel, Chean Bernard Villanueva
+# Pet Class
